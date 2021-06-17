0b974ee8b9e5a2286e6805a22b604ac9075c2935
ismael.ferreira <ismael.dev@gmail.com>
2021-06-17 00:12:20 -0400

    adicionando modulos

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { FormsModule } from '@angular/forms';
+import { DeviceModule } from './device';
+import { CategoryModule } from './category';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
